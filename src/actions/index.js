579cf2fd68bc38f99fa47faedc1f22900610c5dd
Hassan Ali <helfi92@gmail.com>
2016-05-16 10:02:50 -0400

    show post content

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=4505721819';
@@ -13,7 +14,7 @@ export function fetchPosts() {
 		type: FETCH_POSTS,
 		payload: request
 	};
-}
+};
 
 export function createPost(props) {
 	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
@@ -22,4 +23,13 @@ export function createPost(props) {
 		type: CREATE_POST,
 		payload: request
 	}
-}
+};
+
+export function fetchPost(id) {
+	const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+	return {
+		type: FETCH_POST,
+		payload: request
+	}
+};
