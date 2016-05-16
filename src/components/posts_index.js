0fdcf39c2caa48bcca569c0eae534e87735d837e
Hassan Ali <helfi92@gmail.com>
2016-05-15 21:19:22 -0400

    show posts on main page from database

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,28 +2,58 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
 import { Link } from 'react-router';
-
+import { bindActionCreators } from 'redux';
 class PostsIndex extends Component {
 	
 	componentWillMount() {
 		this.props.fetchPosts();
 	}
 
+	renderPosts() {
+		return this.props.posts.map((post) => {
+			return (
+				<li className='list-group-item' key={post.id}>
+					<span className='pull-xs-right'>{post.categories}</span>
+					<strong>{post.title}</strong>
+				</li>
+			)
+		});
+	}
+
 	render(){
 		return (
 			<div>
-				List of blog posts
 				<div className="text-xs-right">
 					<Link to="/posts/new" className="btn btn-primary">
 						Add a post
 					</Link>
 				</div>
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
 				
 			</div>
 			
 		);
 	};
 
+
+
 }
 
-export default connect(null, {fetchPosts})(PostsIndex);
+function mapStateToProps(state) {
+	return {
+		posts: state.posts.all
+	}
+}
+
+function mapDispatchToProps(dispatch){
+	return bindActionCreators({
+		fetchPosts: fetchPosts
+	}, dispatch);
+}
+
+
+//export default connect(null, {fetchPosts})(PostsIndex);
+export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
