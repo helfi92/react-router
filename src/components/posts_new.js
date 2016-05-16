a0a361ea9ed21a5ac0164955bdc835c73d0e1652
Hassan Ali <helfi92@gmail.com>
2016-05-15 20:41:42 -0400

    add validation to form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -8,17 +8,26 @@ class PostsNew extends Component {
 		return (
 			<form onSubmit={handleSubmit(this.props.createPost)}>
 				<h3>Create A New Post</h3>
-				<div className="form-group">
+				<div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
 					<label>Title</label>
 					<input type="text" className="form-control" {...title} />
+					<div className="text-help">
+						{title.touched ? title.error : ''}
+					</div>
 				</div>
-				<div className="form-group">
+				<div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
 					<label>Categories</label>
 					<input type="text" className="form-control" {...categories} />
+					<div className="text-help">
+						{categories.touched ? categories.error : ''}
+					</div>
 				</div>
-				<div className="form-group">
+				<div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
 					<label>content</label>
 					<textarea className="form-control" {...content} />
+					<div className="text-help">
+						{content.touched ? content.error : ''}
+					</div>
 				</div>
 				<button type="submit" className="btn btn-primary">Submit</button>
 			</form>
@@ -31,7 +40,12 @@ function validate(values) {
 
 	if(!values.title) {
 		errors.title = 'Enter a username';
-		
+	}
+	if(!values.categories) {
+		errors.categories = 'Enter categories';
+	}
+	if(!values.content) {
+		errors.content = 'Enter some content';
 	}
 
 	return errors;
